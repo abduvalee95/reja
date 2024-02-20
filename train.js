6a8e4146ca1ff0f8dbeeabb5f9f08ec3323ae2f3
abduvalee95 <abduvalee95@gmail.com>
2024-02-21 07:38:38 +0900

    feat:train.js task F

--- train.js
+++ train.js
@@ -1,3 +1,27 @@
+// **                                                                   Task F chi version
+
+function findDouble(str) {
+    const input = str.split('');
+    for (let i = 0; i < input.length; i++) {
+        if (str.indexOf(input[i], i + 1) !== -1) {
+            return true; // agar i ni qiymati 1 ga oshsa true ni qaytar 
+        }
+    }
+    return false; // !== bu usloviya tekshiradi i -1 ni qaytarmaganini agar -1 qaytarsa false boladi
+}
+
+const double = findDouble("harifntop yuq"); // bosh joyham 2 ta bolsa true qaytaradi
+console.log(double);
+
+/* 
+function findDouble(str) {
+    return str.split('').some((ele, i) => str.indexOf(ele, i + 1) !== -1);
+}
+
+const double = findDouble("helo");
+console.log(double);
+
+ */
 /* // let letter = ['ksddd']
 
 // function counterCreat(a,letter) {
@@ -51,6 +75,8 @@ console.log(`siz yozgan yozuvda "${count1}"dan ${result1} ta mavjud`)
  */
 
 
+
+
 // **                               Task -B
 // function countDigit(str) {
 //     let count = 0; // Raqamlar soni uchun hisoblovchi o'zgaruvchi
@@ -68,26 +94,70 @@ console.log(`siz yozgan yozuvda "${count1}"dan ${result1} ta mavjud`)
 
 // **                             Task optional
 
-/* const xayvonlar = ['tulki','bori','ot','baliq','ayiq']
+// const xayvonlar = ['tulki', 'bori', 'ot', 'baliq', 'ayiq'];
 
-function findAnimal(txt) {
-let idx = xayvonlar.find()
-console.log(idx);
+// function findAnimal(txt, letter) {
+//     const filteredAnimals = txt.filter(animal => animal.startsWith(letter));
+//     return filteredAnimals;
+// }
 
+// console.log(findAnimal(xayvonlar, 'b')); 
+
+
+//const xayvonlar = ['tulki', 'bori', 'ot', 'baliq', 'ayiq'];
+
+// function findAnimal(txt) {
+//     const filteredAnimals = txt.filter(animal => animal.length ===  );
+//     return filteredAnimals;
+// }
+
+// console.log(findAnimal(xayvonlar, 'b'))
+
+/*
+ const xayvonlar = [ 'bori', 'ot', 'baliq', 'ayiq','boyogli','mushuk'];
+
+
+
+function findAnimal(str) {
+
+    const input = str.split('').sort().join('') //join qoshib yozgani
+    // console.log('input',input);
+    return xayvonlar.filter((xayvon )=> {
+    const sortXayvon = xayvon.split('').sort().join('') //join qoshib yozgani
+    for (let x of sortXayvon ) {
+        console.log(x);
+        if (!input.includes(x)){
+            return false;
+        };
+    }
+    return true
+  
+    })
+
+    
 }
-findAnimal('t')
+const javob = findAnimal('qilabtokushum');
+console.log('javob',javob);
 
+ */ 
+//  const xayvonlar = [ 'bori','bodring', 'ot', 'baliq', 'ayiq','boyogli'];
+// function findAnimal(str) {
 
-const indices = [];
-const array = ['ayiq','tulki','jirafa','aew']
-const element = "ayiq";
-let idx = array.indexOf(element);
-while (idx !== -1) {
-  indices.push(idx);
-  idx = array.indexOf(element, idx + 1);
-}
-console.log(indices);
-// [0, 2, 4] */
+//     return xayvonlar.filter((xayvon )=> {
+//     for (const x of xayvon ) {
+//         console.log(x);
+//         if (!str.includes(x)){
+//             return false;
+//         };
+//     }
+//     return true
+//     })
+
+    
+// }
+// const javob = findAnimal('ilgoyob');
+// console.log('javob',javob);
+
 
 // **                           Task -D
 /* 
@@ -137,49 +207,34 @@ console.log(shop);
 
  */
 
-// **                                   Task - G
-
-/* function checkContent(str,str2)  {
-    const content = str.map(str2 => str !== str2)
-    console.log(content);
-}
-checkContent('hi','hi')
-
-function checkContent(str,str2)  {
-
-    let a = str
-    let b = str2 
-    const content = 
-
-}
-checkContent('hi','hi')
- */
-
-/* function checkContent(str1, str2) {
+// **                                                            Task - G
+/* 
+function checkContent(str1, str2) {
     const content = str1 === str2;
     console.log(content);
 
 }
-
 checkContent('salom', 'salom');
  */
-// **                           task G -2 chi version
-/* function checkContent(str1, str2) {
+// **                                                    Task G -2 chi version
+/* 
+function checkContent(str1, str2) {
     const s = str1.split('').map((ele) => {
         return str2.includes(ele)
     });
     console.log(s);
 }
 
-checkContent('hedsllo', 'lloelh');
+checkContent('hedsllo', 'lloesdlh');
 
  */
 
-// **                           task E -2 chi version
+// **                                                        Task E  version
 
 
-const getReverse = (str) => { 
-    return str.split('').reverse()
-    // return str.split('').reverse().join('') join qoshib yozgani
-}
-console.log(getReverse("Assalomu alaykum"));
+// const getReverse = (str) => { 
+//     return str.split('').reverse()
+//     // return str.split('').reverse().join('') //join qoshib yozgani
+// }
+// console.log(getReverse("Assalomu alaykum"));
+
